724b4663cf5d2d0b6e697e1258725f21735e277d
Shahar Shokrani <shokrano@gmail.com>
2018-12-25 20:27:05 +0200

    Basic usage + attribute routing

---
+++ ASPDotNETWebAPI2/Controllers/ContactsController.cs
@@ -0,0 +1,84 @@
+﻿using ASPDotNETWebAPI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPDotNETWebAPI2.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact() {Id=0,FirstName="Peter",LastName="Parker"},
+            new Contact() {Id=1,FirstName="Bruce",LastName="Wayne"},
+            new Contact() {Id=2,FirstName="Bruce",LastName="Banne"}
+        };
+
+        // GET: api/Contacts
+        [Route("api/Contacts")]
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET: api/Contacts/5
+        [Route("api/Contacts/{id:int}")]
+        public IHttpActionResult Get(int id)
+        {
+            Contact contact = contacts.FirstOrDefault<Contact>(c => c.Id == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        [HttpGet]
+        [Route("api/Contacts/{firstName}")]
+        //[ActionName("ContactName")]
+        //public IEnumerable<Contact> GetContactByName(string firstName)
+        public IEnumerable<Contact> FindContactByName(string firstName)
+        {
+            Contact[] contactArray = contacts.Where<Contact>(c => c.FirstName == firstName).ToArray<Contact>();
+
+            return contactArray;
+        }
+
+        // POST: api/Contacts
+        public IEnumerable<Contact> Post([FromBody]Contact newContact)
+        {
+            List<Contact> contactsList = contacts.ToList<Contact>();
+            newContact.Id = contactsList.Count;
+            contactsList.Add(newContact);
+            contacts = contactsList.ToArray();
+
+            return contacts;
+        }
+
+        // PUT: api/Contacts/5
+        public IEnumerable<Contact> Put(int id, [FromBody]Contact changedContact)
+        {
+            Contact contact = contacts.FirstOrDefault<Contact>(c => c.Id == id);
+
+            if (contact != null)
+            {
+                contact.FirstName = changedContact.FirstName;
+                contact.LastName = changedContact.LastName;
+            }
+
+            return contacts;
+        }
+
+        // DELETE: api/Contacts/5
+        public IEnumerable<Contact> Delete(int id)
+        {
+            contacts = contacts.Where<Contact>(c => c.Id != id).ToArray<Contact>();
+            return contacts;
+        }
+    }
+}
